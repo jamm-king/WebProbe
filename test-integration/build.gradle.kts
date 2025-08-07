7e11c1f32711131f1d09c0713da4ddf808c1db89
jamm-king <jmcylove@gmail.com>
2025-08-07 19:04:41 +0900

    test: use Docker MongoDB container in test-integration module test.

--- test-integration/build.gradle.kts
+++ test-integration/build.gradle.kts
@@ -17,7 +17,6 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
-    testRuntimeOnly("de.flapdoodle.embed:de.flapdoodle.embed.mongo.spring30x:4.11.0")
 }
 
 tasks.bootJar { enabled = false }
